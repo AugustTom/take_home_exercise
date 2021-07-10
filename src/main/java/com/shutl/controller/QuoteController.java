12c8c308ac7661da2f07f417040ceaca260c0a25
Auguste <auguste.tomaseviciute@gmail.com>
2021-07-10 14:40:19 +0100

    Add vehicle attribute

--- src/main/java/com/shutl/controller/QuoteController.java
+++ src/main/java/com/shutl/controller/QuoteController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.InvalidKeyException;
+
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -14,7 +16,11 @@ public class QuoteController {
     @RequestMapping(value = "/quote", method = POST)
     public @ResponseBody Quote quote(@RequestBody Quote quote) {
         Long price = Math.abs((Long.valueOf(quote.getDeliveryPostcode(), 36) - Long.valueOf(quote.getPickupPostcode(), 36))/100000000);
-
-        return new Quote(quote.getPickupPostcode(), quote.getDeliveryPostcode(), price);
+        try {
+            price += Math.round(price * quote.getMarkupByVehicle(quote.getVehicle()));
+        } catch (InvalidKeyException e){
+            return new Quote(quote.getPickupPostcode(), quote.getDeliveryPostcode(), "invalid", price);
+        }
+        return new Quote(quote.getPickupPostcode(), quote.getDeliveryPostcode(), quote.getVehicle(), price);
     }
 }
--- src/main/java/com/shutl/model/Quote.java
+++ src/main/java/com/shutl/model/Quote.java
@@ -1,9 +1,30 @@
 package com.shutl.model;
 
+import java.security.InvalidKeyException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quote {
     String pickupPostcode;
     String deliveryPostcode;
     Long price;
+    String vehicle;
+
+    public double getMarkupByVehicle(String vehicle) throws InvalidKeyException {
+        try {
+            return vehicle_markup.get(vehicle);
+        } catch (NullPointerException e){
+            throw new InvalidKeyException("Invalid vehicle input", e.getCause());
+        }
+    }
+
+    private static final Map<String, Double> vehicle_markup = new HashMap<>() {{
+        put("bicycle",0.10);
+        put("motorbike",0.15);
+        put("parcel_car",0.20);
+        put("small_val",0.30);
+        put("large_van",0.40);
+    }};
 
     public Quote() {}
 
@@ -15,6 +36,19 @@ public class Quote {
     public Quote(String pickupPostcode, String deliveryPostcode, Long price) {
         this.pickupPostcode = pickupPostcode;
         this.deliveryPostcode = deliveryPostcode;
+        this.price = price;
+    }
+
+    public Quote(String pickupPostcode, String deliveryPostcode, String vehicle) {
+        this.pickupPostcode = pickupPostcode;
+        this.deliveryPostcode = deliveryPostcode;
+        this.vehicle = vehicle;
+    }
+
+    public Quote(String pickupPostcode, String deliveryPostcode, String vehicle, Long price) {
+        this.pickupPostcode = pickupPostcode;
+        this.deliveryPostcode = deliveryPostcode;
+        this.vehicle = vehicle;
         this.price = price;
     }
 
@@ -40,5 +74,13 @@ public class Quote {
 
     public void setPrice(Long price) {
         this.price = price;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
     }
 }
